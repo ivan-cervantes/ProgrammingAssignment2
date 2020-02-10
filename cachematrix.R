406a5dbd087667fe6c68bb8b28bea71cee1ecaa5
ivan-cervantes <icervantes@lobos.com.mx>
2020-02-09 18:12:29 -0600

    Update cachematrix.R
    Assigment 2.
    It stores an inverse matrix and checks if its stored, otherwise it calculates it.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##Funciton where stores a matrix, caching data for matrix and solved matrix
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(matrix) {
+    x <<- matrix
+    inverse <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setInverse <- function(matrixSolved) { 
+    inverse <- matrixSolved
+  }
+  getInverse <- function() {
+    inverse
+  }
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+##Function to solve a matrix, checks if its stored else solves the the matrix and stores it
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("Getting inversed matrix cached")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$setInverse(inverse)
+  inverse
 }
